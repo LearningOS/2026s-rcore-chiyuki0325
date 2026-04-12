cdb2e35f64bcd1283513773cff3e8a7face53e5f
Kirikaze Chiyuki <me@chyk.ink>
2026-04-12 15:03:13 +0800

    [feat] ported sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,10 +1,11 @@
+use core::mem::size_of;
+
 use crate::{
-    fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    fs::{OpenFlags, open_file},
+    mm::{translated_byte_buffer, translated_ref, translated_refmut, translated_str},
     task::{
-        current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
-        suspend_current_and_run_next, SignalFlags,
-    },
+        SignalFlags, current_process, current_task, current_user_token, exit_current_and_run_next, pid2process, suspend_current_and_run_next
+    }, timer::get_time_us,
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -151,12 +152,36 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!(
         "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().process.upgrade().unwrap().getpid()
     );
-    -1
+    let us = get_time_us();
+
+    // reference: https://rcore-os.cn/rCore-Tutorial-Book-v3/chapter4/6multitasking-based-on-as.html#sys-write
+
+    let token = current_user_token();
+    let ptr = ts as *mut u8;
+    let len = size_of::<TimeVal>();
+    let buffers = translated_byte_buffer(token, ptr, len);
+
+    let result_ts = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    unsafe {
+        let result_ptr = &result_ts as *const TimeVal as *const u8;
+        let mut result_slice = core::slice::from_raw_parts(result_ptr, len);
+        buffers.into_iter().for_each(|part_dst| {
+            let part_size = part_dst.len();
+            let (part_src, slice_remaining) = result_slice.split_at(part_size);
+            part_dst.copy_from_slice(part_src);
+            result_slice = slice_remaining;
+        });
+    }
+    0
 }
 
 /// mmap syscall
